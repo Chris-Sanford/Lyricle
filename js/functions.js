ed7d46c9bb3b2307ff0b3e0cac917b15c16f7b49
Chris Sanford <ChrisSanfordBusiness@gmail.com>
2024-01-02 20:29:58 -0500

    Partially Working Refactor

--- js/functions.js
+++ js/functions.js
@@ -1,7 +1,7 @@
 // functions.js
 
 // hard code a song to guess until we can get the API working
-var song = {
+var songData = {
   title: "All Together Now",
   artist: "The Beatles",
   lyrics: `One, two, three, four
@@ -9,7 +9,21 @@ Can I have a little more?
 Five, six, seven, eight, nine, ten, I love you`
 }
 
-function getSong() { // Ask the user to choose a song of their choice
+// construct/declare a class called Song that will contain the original data and the properties/values that we calculate for the game
+class Song {
+  constructor(title, artist, lyrics) {
+    this.title = title;
+    this.artist = artist;
+    this.lyrics = lyrics;
+    this.lines = lyrics.split("\n"); // Split the secret string into lines separated by new lines
+    this.words = lyrics.replace(/[^a-zA-Z0-9\n\s]/g, "").toLowerCase().split(/\s+/); // Remove all special characters except \n and make all lowercase
+    this.formattedLyrics = this.words.join(" "); // Join the words back into a string with spaces
+    this.formattedLines = lyrics.replace(/[^a-zA-Z0-9\n\s]/g, "").toLowerCase().split("\n");
+    this.formattedWords = lyrics.replace(/[^a-zA-Z0-9\n\s]/g, "").toLowerCase().split(/\s+/);
+  }
+}
+
+function getSong() { // Ask the user to choose a song
   // get the div element getSong from the HTML document and set it to the variable named container so we can manipulate it
   var container = document.getElementById("getSong");
 
@@ -24,65 +38,12 @@ function getSong() { // Ask the user to choose a song of their choice
 
   var button = document.createElement("button"); // Create a button element
   button.innerHTML = "Select Song"; // Define the text within the button to label it
-  button.addEventListener("click", generateSong); // Add event listener to the button so it responds to clicks and calls the generateSong function (so the song can be selected and loaded in the future)
+  button.addEventListener("click", startGame); // Add event listener to the button so it responds to clicks and calls the startGame function (so the song can be selected and loaded in the future)
   container.appendChild(button); // append the button to the div
 }
 
-function generateSong() { // Loads main game with song lyrics to guess
-  document.getElementById("songLyrics").innerHTML = ""; // Clear the songLyrics div
-  document.getElementById("resultsMessage").innerHTML = ""; // Clear the resultsMessage div
-  document.getElementById("score").innerHTML = ""; // Clear the score div
-  var lines = song.lyrics.split("\n"); // Split the secret string into lines separated by new lines
-  console.log(lines)
-  var words = lines.split(/\s+/); // Split the secret string into words separated by spaces
-  console.log(words)
-
-
-  // WHERE WE LEFT OFF: create a complete array of all words formatted for later comparison
-  var formattedLyrics = lines.replace(/[^a-zA-Z ]/g, "").toLowerCase(); // Remove all special characters and make all lowercase
-  console.log(formattedLyrics);
-  var container = document.getElementById("songLyrics"); // Get the songLyrics div
-  
-
-  // Create a div to hold the song title and artist
-  var titleDiv = document.createElement("div"); // create a div element
-  titleDiv.innerHTML = "<b>" + song.title + "</b> by " + song.artist; // populate the div with the song title and artist
-  container.appendChild(titleDiv); // append the div to the container div
-  // append a line break to the container div to space out the title and artist from the lyrics
-  container.appendChild(document.createElement("br"));
-
-  var wordIndex = 0; // Keep track of the word index, sets to 0 to start
-  var maxWidth = 100; // Define a maximum width for the input boxes (should 100 be the value? will any realistic word require more pixels than this?)
-  lines.forEach(function (line) { // executes a function against each line from lines array (can we port this to a legit named function?)
-    var lineWords = line.split(/\s+/); // Split the line into words
-    lineWords.forEach(function (word) { // executes a function against each word from lineWords array
-      var input = document.createElement("input"); // creates an input element
-      input.type = "text"; // makes the element a text input
-      input.id = "myInput" + wordIndex; // defines the unique id of the input element based on the index of the word in the secret string
-      var width = Math.min(10 * word.length, maxWidth); // defines variable width using the Math.min static method to set the value to either the length of the word * 10
-      // or the maxWidth value, whichever is smaller. This means a word with more than 10 characters will be restricted to the maxWidth value.
-      input.style.width = width + "px"; // width needs to be defined in px (pixels) so we add the px string to the end of the width value
-      input.style.textAlign = "center"; // center the text within the input box
-      input.addEventListener("input", (function(input, wordIndex) { // adds event listener so any time there is input into the input box, it executes the function
-        return function() { // calls the function defined below and returns parent function from addEventListener
-          wordboxInputListener(input, wordIndex); // executes the wordboxInputListener function with relevant parameters/arguments
-        } // we must be able to simplify this nesting craziness
-      })(input, wordIndex)); // ensures the correct input and wordIndex values are passed to the wordboxInputListener function
-      container.appendChild(input); // appends the input element to the container div so it's populated in the HTML document
-      wordIndex++; // increments the wordIndex value by 1
-    });
-    container.appendChild(document.createElement("br")); // adds a line break after each line of the song
-  });
-
-  container.addEventListener("keyup", function (event) { // adds event listener for key input on wordbox
-    if (event.key === "Enter") { // if the key pressed is the Enter key
-      submit(); // call the submit function
-    }
-  });
-}
-
-function wordboxInputListener(input, wordIndex) { // Event listener function for lyric input boxes
-  updateColor(); // call the updateColor function
+function wordboxInputListener(input, song, wordIndex) { // Event listener function for lyric input boxes
+  updateColor(song, wordIndex); // call the updateColor function
   if (input.style.backgroundColor === "green") { // if the words matched, the input is correct, and the background color of the wordbox is green
     var nextInput = document.getElementById("myInput" + (wordIndex + 1)); // get next input box element by ID using current index + 1
     if (nextInput) { // if there is a next input box (i.e. we're not at the end of the song)
@@ -91,17 +52,80 @@ function wordboxInputListener(input, wordIndex) { // Event listener function for
   }
 }
 
-function updateColor() { // Update the color of the lyric input boxes based on guess correctness
-  var input = event.target; // this is working but deprecated but how do we replace this?
-  var inputIndex = parseInt(input.id.replace("myInput", "")); // get the index of the input box (there must be a better way to do this)
+function updateColor(song, wordIndex) { // Update the color of the lyric input boxes based on guess correctness
+  var input = event.target; // this is working but deprecated- how do we replace this?
   var formattedInput = input.value.replace(/[^a-zA-Z ]/g, "").toLowerCase(); // from input, remove all punctuation and make all lowercase
-  var comparisonWord = song.formattedLyrics // set the comparisonWord to a variable (there must be a better way to do this)
-    .split(/\s+/); // Split the secret string into words (haven't we already done this?)
+  var comparisonWord = song.formattedLyrics[wordIndex] // set the comparisonWord to a variable 
   if (formattedInput === comparisonWord) {
     input.style.backgroundColor = "green"; // Set background color to green if input matches the corresponding word in the secret string
   } else {
     input.style.backgroundColor = "yellow"; // Set background color to red if input does not match the corresponding word in the secret string
   }
+}
+
+function constructInputBoxes(song, line, container) { // Construct the input boxes for the song lyrics
+  var wordIndex = 0; // Keep track of the word index, sets to 0 to start
+  var maxWidth = 100; // Define a maximum width for the input boxes (should 100 be the value? will any realistic word require more pixels than this?)
+
+  var lineWords = line.split(/\s+/); // Split the line into words
+  lineWords.forEach(function (word) { // executes a function against each word from lineWords array
+    var input = document.createElement("input"); // creates an input element
+    input.type = "text"; // makes the element a text input
+    input.id = "myInput" + wordIndex; // defines the unique id of the input element based on the index of the word in the secret string
+    var width = Math.min(10 * word.length, maxWidth); // defines variable width using the Math.min static method to set the value to either the length of the word * 10
+    // or the maxWidth value, whichever is smaller. This means a word with more than 10 characters will be restricted to the maxWidth value.
+    input.style.width = width + "px"; // width needs to be defined in px (pixels) so we add the px string to the end of the width value
+    input.style.textAlign = "center"; // center the text within the input box
+
+    input.addEventListener("input", (function(input, wordIndex) { // adds event listener so any time there is input into the input box, it executes the function
+      return function() { // calls the function defined below and returns parent function from addEventListener
+        wordboxInputListener(input, song, wordIndex); // executes the wordboxInputListener function with relevant parameters/arguments
+      }
+    })(input, wordIndex)); // ensures the correct input and wordIndex values are passed to the wordboxInputListener function
+
+    container.appendChild(input); // appends the input element to the container div so it's populated in the HTML document
+    wordIndex++; // increments the wordIndex value by 1
+  });
+  
+  container.appendChild(document.createElement("br")); // adds a line break after each line of the song
+}
+
+function startGame() { // Loads main game with song lyrics to guess
+  document.getElementById("songLyrics").innerHTML = ""; // Clear the songLyrics div
+  document.getElementById("resultsMessage").innerHTML = ""; // Clear the resultsMessage div
+  document.getElementById("score").innerHTML = ""; // Clear the score div
+
+  // construct a new Song object using the songData object
+  var song = new Song(songData.title, songData.artist, songData.lyrics);
+  console.log(song); // log the song object to the console
+
+  var container = document.getElementById("songLyrics"); // Get the songLyrics div
+
+  // Create a div to hold the song title and artist
+  var titleDiv = document.createElement("div"); // create a div element
+  titleDiv.innerHTML = "<b>" + song.title + "</b> by " + song.artist; // populate the div with the song title and artist
+  container.appendChild(titleDiv); // append the div to the container div
+  // append a line break to the container div to space out the title and artist from the lyrics
+  container.appendChild(document.createElement("br"));
+
+  // construct the input boxes for the song lyrics to start the game
+  song.lines.forEach(function (line) {constructInputBoxes(song, line, container)});
+
+  container.addEventListener("keyup", function (event) { // adds event listener for key input on wordbox
+    if (event.key === "Enter") { // if the key pressed is the Enter key
+      submit(); // call the submit function
+    }
+  });
+}
+
+function scoreSong(formattedInputs, comparisonWords) { // Calculate the number of words guessed correctly
+  var score = 0; // default to 0 for current score
+  formattedInputs.forEach(function (input, index) { // for each formattedInput
+    if (input === comparisonWords[index]) { // if the formattedInput matches the comparisonWord at the same index
+      score++; // add +1 to the current score. Didn't we do these correctness checks already?
+    }
+    document.getElementById("score").innerHTML = score + " Words Correct!"; // populate the score div with the final score
+  });
 }
 
 function submit() { // submit the guessed lyrics and calculate and return score
@@ -147,20 +171,9 @@ function submit() { // submit the guessed lyrics and calculate and return score
   }
 }
 
-function scoreSong(formattedInputs, comparisonWords) { // Calculate the number of words guessed correctly
-  var currentscore = 0; // default to 0 for current score
-  formattedInputs.forEach(function (input, index) { // for each formattedInput
-    if (input === comparisonWords[index]) { // if the formattedInput matches the comparisonWord at the same index
-      currentscore++; // add +1 to the current score. Didn't we do these correctness checks already?
-    }
-    var finalscore = currentscore; // set a finalScore variable. Is this necessary? Can we just make one var called score?
-    document.getElementById("score").innerHTML = finalscore + " Words Correct!"; // populate the score div with the final score
-  });
-}
-
 function init () { // Initialize the game
   //getSong();
-  generateSong();
+  startGame();
 }
 
-window.onload = init; // upon loading the page, initialize the game
+window.onload = init; // upon loading the page, initialize the game9
