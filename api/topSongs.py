432ea2e6d40a8ae1b82b7cf19063022d79a04b1a
Chris Sanford <ChrisSanfordBusiness@gmail.com>
2024-02-08 08:41:08 -0500

    Obtain Top Songs from Spotify

---
+++ api/topSongs.py
@@ -0,0 +1,64 @@
+'''
+This script is meant to be run on your local machine. It will save the top songs from a Spotify playlist to a JSON file. genius.py will use this JSON file to obtain a list of song lyrics to play with.
+'''
+
+import os
+import json
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+#region Global Variable Declarations
+service = 'spotify'
+keyFileName = f'secrets/{service}_client_secret.key'
+jsonFileName = f'gameData/topSongs.json'
+#endregion Global Variable Declarations
+
+#region Functions
+def get_client_secret(keyFileName):
+    try:
+        with open(keyFileName, 'r') as file:
+            secret = file.read().strip()
+            return secret
+            if secret == 'YOUR_SECRET':
+                print("{service} API Client Access Token not set. Please update the value of {keyFileName} with the {service} api client secret.")
+                exit()
+    except FileNotFoundError:
+        print("The secrets directory does not exist.")
+        # Create the secrets directory
+        os.mkdir('secrets')
+        print("The secrets directory has been created.")
+        # Create the genius_client_secret.key file
+        with open(keyFileName, 'w') as file:
+            file.write('YOUR_SECRET')
+        print("The {keyFileName} file has been created. Please add your {service} API access token.")
+        exit()
+    except Exception as e:
+        print("An error occurred while reading the access token file:", e)
+        exit()
+#endregion Functions
+        
+#region Execution
+secret = get_client_secret(keyFileName)
+
+# Set up Spotify client
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
+    client_id='bdfb243246774f4d8f36386130717123',
+    client_secret=secret,
+    redirect_uri='http://localhost:8888/callback/',
+    scope='playlist-read-private'))
+
+# ID of the public playlist of Spotify's most streamed songs
+playlist_id = '2YRe7HRKNRvXdJBp9nXFza'
+
+# Get playlist data
+results = sp.playlist(playlist_id)
+
+# Get track data
+tracks = results['tracks']
+
+# Save track data to JSON file
+with open(jsonFileName, 'w') as f:
+    json.dump(tracks, f)
+
+print("Playlist tracks saved to '{jsonFileName}'")
+#endregion Execution
