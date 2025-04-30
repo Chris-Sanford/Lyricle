8ac3edd71745ecdfbcf8cb554dfbb7d075f4fe2f
Chris Sanford <ChrisSanfordBusiness@gmail.com>
2025-04-29 20:30:55 -0400

    Attempt 02

---
+++ deploy.js
@@ -0,0 +1,5 @@
+// This is a placeholder script that simply succeeds.
+// Since we're deploying static files directly from the public directory,
+// we don't need any build process.
+console.log('Deploying static files from public directory...');
+process.exit(0); 
